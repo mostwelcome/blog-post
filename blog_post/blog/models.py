a9e598dd22a7c4e0ed30062d83d95bdae4a6066e
mostwelcome <tuadutta13@gmail.com>
2021-08-31 10:11:26 +0530

    added relations for post,author and tags

--- blog_post/blog/models.py
+++ blog_post/blog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.caption
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email_add = models.EmailField(max_length=100)
+
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def __str__(self):
+        return self.full_name()
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    excerpt = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, null=True, on_delete=models.SET_NULL, related_name="post")
+    image_name = models.CharField(max_length=100)
+    date = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(default="", null=False, blank=True, unique=True, db_index=True)
+    content = models.TextField()
+    tag = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.title
